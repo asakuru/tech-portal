b436be3ca51457a27d827c4428b02539ad1791e3
Fuzzy Solutions, LLC <ghostlaw@gmail.com>
2026-01-29 21:13:55 -0500

    UI: Fixed input styling in dark mode (v1.3)

--- entry.php
+++ entry.php
@@ -414,7 +414,7 @@ else {
 <head>
     <title>Job Entry</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="style.css?v=1.1">
+    <link rel="stylesheet" href="style.css?v=1.3">
     <link rel="icon" type="image/png" href="favicon.png?v=2">
     <link rel="shortcut icon" href="favicon.ico?v=2">
     <link rel="apple-touch-icon" href="favicon.png">
